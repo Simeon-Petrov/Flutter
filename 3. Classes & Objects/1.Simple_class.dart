dddadada1cdcd350c0ae0921dd1302380ec068b5
Simeon-Petrov <simeon.p.petrov@abv.bg>
2025-04-08 03:27:54 +0300

    add exercise from 3

--- 3. Classes & Objects/1.Simple_class.dart
+++ 3. Classes & Objects/1.Simple_class.dart
@@ -1,16 +1,16 @@
 import 'dart:vmservice_io';
 
+void main() {
+  Person person = Person("Simeon", 27);
+  person.introduce();
+}
+
 class Person {
-  String name;
-  int age;
+  String? name;
+  int? age;
 
   Person(this.name, this.age);
   void introduce() {
     print("Hi my name is $name and I am $age old.");
   }
-}
-
-void main() {
-  Person person = Person("Simeon", 27);
-  person.introduce();
 }
